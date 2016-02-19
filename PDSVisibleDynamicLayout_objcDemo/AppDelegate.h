67e929c84b4df1e8e6a786014fb7c6480bedbd15
w91379137 <yclin@gmail.com>
2016-02-19 17:22:26 +0800

    move files

---
+++ PDSVisibleDynamicLayout_objcDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDSVisibleDynamicLayout_objcDemo
+//
+//  Created by w91379137 on 2016/2/19.
+//  Copyright © 2016年 w91379137. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDSVisibleDynamicLayout_objcDemo/PDSVisibleDynamicLayout_objc/UIView+PairCopyLayout.h
@@ -0,0 +1,21 @@
+//
+//  UIView+PairCopyLayout.h
+//  PDSVisibleDynamicLayout_objcDemo
+//
+//  Created by w91379137 on 2016/2/19.
+//  Copyright © 2016年 w91379137. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (PairCopyLayout)
+
+- (NSArray *)pairCopyArray1:(NSArray *)array1
+                     Array2:(NSArray *)array2;
+
+- (NSArray *)copyFromView:(UIView *)view1
+                   ToView:(UIView *)view2
+                   array1:(NSArray *)array1
+                   array2:(NSArray *)array2;
+
+@end
